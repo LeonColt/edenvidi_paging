11c667748c34e38da3f085ef385cbc785a7b674c
leoncolt <lionmanbloodsucker@yahoo.com>
2019-08-03 18:22:16 +0700

    export paging

--- lib/edenvidi_paging.dart
+++ lib/edenvidi_paging.dart
@@ -3,6 +3,7 @@ library edenvidi_paging;
 export 'src/page_info.dart';
 export 'src/definitions.dart';
 
+export 'src/Paging.dart';
 export 'src/paging_controller.dart';
 export 'src/rest_paging_controller.dart';
 export 'src/graphql_paging_controller.dart';
